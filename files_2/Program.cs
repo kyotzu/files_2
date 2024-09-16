4517e2b304ef80db4ceb2587429a65fa2d98c76c
kyotzu <konstantinppv12@gmail.com>
2024-09-16 14:42:00 +0500

    Добавьте файлы проекта.

---
+++ files_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к директории:");
+        string directoryPath = Console.ReadLine();
+
+        try
+        {
+            long folderSize = GetDirectorySize(directoryPath);
+            Console.WriteLine($"Размер папки: {folderSize} байт");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+
+    static long GetDirectorySize(string directoryPath)
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            throw new DirectoryNotFoundException($"Директория по пути {directoryPath} не найдена.");
+        }
+
+        long totalSize = 0;
+
+        DirectoryInfo dirInfo = new DirectoryInfo(directoryPath);
+        FileInfo[] files = dirInfo.GetFiles();
+        foreach (FileInfo file in files)
+        {
+            totalSize += file.Length;
+        }
+
+        DirectoryInfo[] directories = dirInfo.GetDirectories();
+        foreach (DirectoryInfo directory in directories)
+        {
+            totalSize += GetDirectorySize(directory.FullName);
+        }
+
+        return totalSize;
+    }
+}
